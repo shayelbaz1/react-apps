81d8d005221be908f701b2f015703eae7a80d474
Shay <shaysha6@gmail.com>
2021-03-23 11:34:05 +0200

    test1.2

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,7 @@ class App extends Component {
         return (
             <Provider store={store()}>
                 <Router>
-                    test1.1
+                    test1.2
                     <Routes />
                 </Router>
             </Provider>
