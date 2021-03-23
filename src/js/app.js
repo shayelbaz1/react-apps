92c8203181a35fa34c5165fc4e51aab1dafeb0ff
Shay <shaysha6@gmail.com>
2021-03-23 12:08:45 +0200

    finish tags

--- src/js/app.js
+++ src/js/app.js
@@ -12,7 +12,6 @@ class App extends Component {
         return (
             <Provider store={store()}>
                 <Router>
-                    test1.0.1
                     <Routes />
                 </Router>
             </Provider>
